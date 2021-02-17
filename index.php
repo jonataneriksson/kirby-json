10dcd4530d1703c1e8e54d06c48b96ae06a96aed
Jonatan Eriksson <jonatan.eriksson@gmail.com>
2021-02-17 15:33:05 +0100

    Hmmm 100 lines more code..

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 Kirby::plugin('jonataneriksson/json', [
-  'options' => ['cache' => true],
+  'options' => [
+    'cache' => true
+  ],
   'hooks' => [
     'site.update:after' => function () {
         kirby()->cache('jonataneriksson.json')->flush();
@@ -35,8 +37,9 @@ Kirby::plugin('jonataneriksson/json', [
         'action'  => function () {
 
           $apiCache = kirby()->cache('jonataneriksson.json');
-          $cacheName = get('path');
+          $cacheName = get('language') . "/" . get('path');
           $apiData = $apiCache->get($cacheName);
+          //$apiData = null;
 
           if ($apiData === null || get('debug') ) {
 
@@ -44,8 +47,7 @@ Kirby::plugin('jonataneriksson/json', [
             $json = (object)[];
 
             //Some GET variables are needed
-            $timer = get('debug') ? microtime(true) : false;
-            $GLOBALS['debug'] = (array)[];
+            $timer = microtime(true);
             $language = get('language');
 
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -85,14 +87,11 @@ Kirby::plugin('jonataneriksson/json', [
                 }
             }
 
-            //Field can be yaml
-            function canbeyaml($field)
-            {
-              try {
-                  return $field->yaml();
-              } catch (Exception $exception) {
-                  return false;
-              }
+            //String is JSON
+            function stringIsJson($string) {
+              if(is_numeric($string)) return false;
+              json_decode((string)$string);
+              return (json_last_error() == JSON_ERROR_NONE);
             }
 
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -100,14 +99,79 @@ Kirby::plugin('jonataneriksson/json', [
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
             function getfield($field) {
-              $current_field = [];
+              $return = [];
               if (fieldisyaml($field)) {
-                $current_field = getstructure($field);
+                $return = getstructure($field);
+              } elseif(stringIsJson($field->value)) {
+                $jsonvalue = json_decode($field->value);
+                if ( gettype($jsonvalue) == 'array' ) {
+                  $return[$field->key()] = getarray(json_decode($field->value));
+                } elseif(gettype($jsonvalue) == 'string' ) {
+                  $return[$field->key()] = $jsonvalue;
+                }
               } else {
-                $current_field['value'] = $field;
-                $current_field['kirbytext'] = kirbytext($field);
+                $return['value'] = $field->value;
+                $return['kirbytext'] = kirbytext($field->value);
               }
-              return $current_field;
+              return $return;
+            }
+
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* !Get field */
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            function checkkey($key) {
+              if (substr($key, 0, 1) === '_') {
+                return false;
+              }
+              if ($key === 'id') {
+                return false;
+              }
+              return true;
+            }
+
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* !Get Array */
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            function getarray($input) {
+              $return = [];
+              foreach($input as $key => $value):
+                if(gettype($value) == 'bool') {
+                  print_r('NULL!');
+                }
+                if ( (gettype($value) == 'string' || gettype($value) == 'double') && checkkey($key) ) {
+                  if(stringIsJson($value)) {
+                    $jsonvalue = json_decode($value);
+                    if ( gettype($jsonvalue) == 'array' ) {
+                      $return[$key] = getarray(json_decode($value));
+                    } elseif(gettype($jsonvalue) == 'string' ) {
+                      $return[$key] = $jsonvalue;
+                    }
+                  } else {
+                    $return[$key]['value'] = $value;
+                    $return[$key]['kirbytext'] = kirbytext($value);
+                  }
+                } elseif ( gettype($value) == 'array' && checkkey($key)  ) {
+                  $return[$key] = getarray($value);
+                } else {
+                  //Heres id & _key & _uid
+                  $return[$key] = $value;
+                }
+              endforeach;
+              return $return;
+            }
+
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* !Get structure */
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            function getstructure($input) {
+              $return = [];
+              foreach($input->toStructure() as $fieldgroup):
+                $return[] = getarray($fieldgroup->toArray());
+              endforeach;
+              return $return;
             }
 
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -117,7 +181,7 @@ Kirby::plugin('jonataneriksson/json', [
             function getfields($page) {
               $contentitem = [];
               foreach($page->content(get('language'))->data() as $key => $field):
-                $contentitem[$key] = getfield($field);
+                $contentitem[$key] = getfield( $page->content(get('language'))->get($key) );
               endforeach;
               return $contentitem;
             }
@@ -129,18 +193,14 @@ Kirby::plugin('jonataneriksson/json', [
             function getpagestructures($pages) {
 
               //Let's make the return array
-              $pageitems = (array)[];
-
+              $pageitems = false;
               $index = 0;
 
               //Loop through pages
               foreach($pages as $page):
-
                 //Save page data to array
                 $pageitems[$page->uid()] = getpagestructure($page, $index);
-
                 $index++;
-
               endforeach;
 
               //Return pages array
@@ -183,8 +243,65 @@ Kirby::plugin('jonataneriksson/json', [
               $pageitem->strings = (array)$page->content(get('language'))->toArray();
               $pageitem->template = (string)$page->intendedTemplate();
               $pageitem->folder = (string)$page->contentURL();
+              $pageitem->children = getpagestructures($page->children());
               $pageitem->extended = true;
+              $pageitem->index = (int) $page->num();
+              $pageitem->next = ($page->hasNext()) ? $page->next()->url() : false;
               return $pageitem;
+            }
+
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* !Should parent be extended?*/
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            function loadparent($page) {
+              if ($page->children()) {
+                foreach($page->children() as $child):
+                  if( get('path') == (string)$child->uri() ):
+                    return true;
+                  endif;
+                endforeach;
+              }
+              return false;
+            }
+
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* !Should children be extended?*/
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            function loadchildren($page) {
+              if ($page->parent()) {
+                if( get('path') == $page->parent()->uri() ):
+                  return true;
+                endif;
+              }
+              return false;
+            }
+
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+            /* !Should page be extended?*/
+            /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+            function shouldbeextended($page) {
+              if( get('path')==(string)$page->uri() ):
+                return true;
+              elseif( get('full') ):
+                return true;
+              elseif( loadparent($page) ):
+                return true;
+              elseif( loadchildren($page) ):
+                return true;
+              endif;
+
+              if( get('require') ):
+                $requiredpages = explode(',',get('require'));
+                foreach($requiredpages as $requiredpage):
+                  if( $requiredpage == (string)$page->uri() ):
+                    return true;
+                  endif;
+                endforeach;
+              endif;
+              return false;
             }
 
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -199,19 +316,18 @@ Kirby::plugin('jonataneriksson/json', [
               $pageitem->url = (string)$page->url();
               $pageitem->uid = (string)$page->uid();
               $pageitem->visible = (string)$page->isListed();
+              $pageitem->strings = (array)$page->content(get('language'))->toArray();
 
               //Setup children
-              if($page->hasChildren() && !get('structure')):
+              if($page->hasChildren()):
                 $pageitem->children = getpagestructures($page->children());
               else:
                 $pageitem->children = false;
               endif;
 
               //Extend page item
-              if(get('path')==(string)$page->uri() || get('full')):
+              if(shouldbeextended($page)):
                   $pageitem = extendpage($page, $pageitem);
-              elseif('portfolio'==(string)$page->uri()):
-                      $pageitem = extendpage($page, $pageitem);
               else:
                   $pageitem->extended = false;
               endif;
@@ -231,26 +347,27 @@ Kirby::plugin('jonataneriksson/json', [
               $pageitem->uri = (string)$page->uri();
               $pageitem->url = (string)$page->url();
               $pageitem->uid = (string)$page->uid();
+              $pageitem->parent = (string)$page->parent();
               $pageitem->visible = (string)$page->isListed();
+              $pageitem->index = $index;
 
               //Get strings only.
               if(get('language')) $pageitem->language = get('language');
               $pageitem->strings = (array)$page->content(get('language'))->toArray();
 
               //Setup children
-              if($page->hasChildren() && !get('structure')):
+              if( ($page->hasChildren() && get('structure')==1) || (get('full')==1) ):
                 $pageitem->children = getpages($page->children());
+              else:
+                $pageitem->children = false;
               endif;
 
               //Extend page item
-              //if(get('path')==(string)$page->uri() || get('full')):
+              if(shouldbeextended($page)):
                   $pageitem = extendpage($page, $pageitem);
-
-                  //Add some meta
-                  $pageitem->index = $index;
-              //else:
-              //    $pageitem->extended = false;
-              //endif;
+              else:
+                  $pageitem->extended = false;
+              endif;
 
               //Return page array
               return $pageitem;
@@ -296,12 +413,11 @@ Kirby::plugin('jonataneriksson/json', [
               endif;
 
               if($fileitem['type'] == 'video'):
-                $stillfromvideo = $file->thumb(['clip' => true, 'still' => true]);
-                $fileitem['height'] = (string)$stillfromvideo->height();
-                $fileitem['width'] = (string)$stillfromvideo->width();
-                $fileitem['ratio'] = (string)round($stillfromvideo->ratio()*100)/100;
-                $fileitem['orientation'] = (string)$stillfromvideo->orientation();
-                $fileitem['thumbnails'] = getthumbnails($file, ['clip' => true, 'still' => true]);
+                //$stillfromvideo = $file->thumb(['clip' => true, 'still' => true]);
+                //$fileitem['height'] = (string)$stillfromvideo->height();
+                //$fileitem['width'] = (string)$stillfromvideo->width();
+                //$fileitem['ratio'] = (string)round($stillfromvideo->ratio()*100)/100;
+                //$fileitem['orientation'] = (string)$stillfromvideo->orientation();
               endif;
 
               foreach($file->content(get('language'))->data() as $key => $value):
@@ -322,14 +438,13 @@ Kirby::plugin('jonataneriksson/json', [
                 $id =  'w' . $width ;
                 $thumbnails[$id] = (string)$file->thumb(['width' => $width])->url();
               endforeach;
-              $placeholderfile = $file->thumb(['width' => 15, 'quality' => 40]);
-              $placeholderfile->publish();
-              $placeholderpath = Url::path($placeholderfile->url());
-              $placeholderurl = kirby()->site()->url() . '/' . $placeholderpath;
-              $placeholder = $placeholderfile->base64();
-              $thumbnails['placeholder'] = $placeholder;
+              //$placeholderfile = $file->thumb(['width' => 15, 'quality' => 40]);
+              //$placeholderfile->publish();
+              //$placeholderpath = Url::path($placeholderfile->url());
+              //$placeholderurl = kirby()->site()->url() . '/' . $placeholderpath;
+              //$placeholder = $placeholderfile->base64();
+              //$thumbnails['placeholder'] = $placeholder;
               $filename = $file->name();
-              $base64lenght = strlen ($placeholder);
               return $thumbnails;
             }
 
@@ -337,25 +452,30 @@ Kirby::plugin('jonataneriksson/json', [
             /* !The Return */
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-            $json->debug = false;
+            //Language
+            $json->language = get('language');
 
+            //Site
             $json->site = kirby()->site()->content(get('language'))->toArray();
 
+            //Page
             if(get('path')):
               if($page = kirby()->site()->pages()->findByURI(get('path'))):
                 $json->page = getpage($page);
               endif;
             endif;
 
+            //Pages
             $json->pages = getpages(kirby()->site()->pages());
-            if(get('debug')) $json->debug = $GLOBALS['debug'];;
-            //Timer for debugging.
-            if($timer) $json->loadtime = microtime(true) - $timer;
 
+            //Timer
+            $json->loadtime = microtime(true) - $timer;
+
+            //Retrun from cache
             $apiCache->set($cacheName, json_encode($json), 30);
           }
 
-          return $apiCache->get($cacheName);
+          return Response::json($apiCache->get($cacheName));
         }
       ]
     ];
